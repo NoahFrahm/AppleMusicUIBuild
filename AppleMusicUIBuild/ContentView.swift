3a81885942a14382281db10889b70333c00579ee
NoahFrahm <nofrahm@live.unc.edu>
2022-02-15 18:50:12 -0500

    Initial Commit

---
+++ AppleMusicUIBuild/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusicUIBuild
+//
+//  Created by Noah Frahm on 2/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
